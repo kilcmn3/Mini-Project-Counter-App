6b87a8648e1ccda28b16db5e14efa73a8f443032
kilcmn <kilcmn3@hotmail.com>
2020-07-30 02:59:16 -0400

    fail to apply closure

--- src/ButtonComponent.js
+++ src/ButtonComponent.js
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from 'react';
 
-function ButtonComponent() {
+const ButtonComponent = () => {
+  const [count, setCount] = useState(0);
+
+  const handleDecrease = () => {
+    if (count === 0) {
+      setCount(0);
+    } else {
+      setCount(count - 1);
+    }
+  };
   return (
     <div>
-      <h1>hello</h1>
+      {count}
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => handleDecrease()}>Decrease</button>
     </div>
   );
-}
-
+};
 export default ButtonComponent;
--- src/MainComponent.js
+++ src/MainComponent.js
@@ -1,24 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ButtonComponent from './ButtonComponent';
-class MainComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-    };
-  }
 
-  handleClick = (event) => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <ButtonComponent />
-      </div>
-    );
-  }
-}
+const MainComponent = () => {
+  return (
+    <div>
+      <ButtonComponent />
+      <ButtonComponent />
+      <ButtonComponent />
+      <ButtonComponent />
+    </div>
+  );
+};
 
 export default MainComponent;
