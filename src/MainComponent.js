06f5d90c0763e9ecce1585eac70e1fee0b0f6b26
kilcmn <kilcmn3@hotmail.com>
2020-08-14 19:41:05 -0400

    can't think of better way with using hooks

--- src/MainComponent.js
+++ src/MainComponent.js
@@ -2,14 +2,23 @@ import React, { useState, useEffect } from 'react';
 import ButtonComponent from './ButtonComponent';
 
 const MainComponent = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleClick = () => {
+    // let child component that all zero button has been triggered.
+    // set 'count' back to zero ,
+    // toggle back the button to false
+    setToggle(true);
+  };
+
   return (
     <div>
-      <button>All zero</button>
+      <button onClick={handleClick}>All zero</button>
       <button disabled={false}>Reset All button</button>
-      <ButtonComponent />
-      <ButtonComponent />
-      <ButtonComponent />
-      <ButtonComponent />
+      <ButtonComponent toggle={toggle} />
+      <ButtonComponent toggle={toggle} />
+      <ButtonComponent toggle={toggle} />
+      <ButtonComponent toggle={toggle} />
     </div>
   );
 };
