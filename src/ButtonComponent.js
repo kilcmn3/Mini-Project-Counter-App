69726e20053f8ed761b42f1d89585887abc6139a
kilcmn <kilcmn3@hotmail.com>
2020-08-15 12:17:53 -0400

    changing from hooks to class

--- src/ButtonComponent.js
+++ src/ButtonComponent.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
 const ButtonComponent = (props) => {
-  const [count, setCount] = useState(0);
-  const [remove, setRemove] = useState(true);
-
   const handleDecrease = () => {
     if (count === 0) {
       setCount(0);
@@ -15,15 +12,13 @@ const ButtonComponent = (props) => {
   const handleRemove = () => {
     setRemove(false);
   };
-  return remove ? (
+  return (
     <div>
       {count}
       <button onClick={() => setCount(count + 1)}>Increase</button>
       <button onClick={() => handleDecrease()}>Decrease</button>
       <button onClick={() => handleRemove()}>Remove Button</button>
     </div>
-  ) : (
-    false
   );
 };
 export default ButtonComponent;
--- src/MainComponent.js
+++ src/MainComponent.js
@@ -1,27 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import ButtonComponent from './ButtonComponent';
 
-const MainComponent = () => {
-  const [toggle, setToggle] = useState(false);
-
-  const handleClick = () => {
-    // let child component that all zero button has been triggered.
-    // set 'count' back to zero ,
-    // toggle back the button to false
-    setToggle(!toggle);
-  };
-
-  return (
-    <div>
-      {toggle}
-      <button onClick={handleClick}>All zero</button>
-      <button disabled={false}>Reset All button</button>
-      <ButtonComponent toggle={toggle} />
-      <ButtonComponent toggle={toggle} />
-      <ButtonComponent toggle={toggle} />
-      <ButtonComponent toggle={toggle} />
-    </div>
-  );
-};
+class MainComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: [
+        { id: 0, count: 0 },
+        { id: 1, count: 1 },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div>
+        <button>All zero</button>
+        <button disabled={false}>Reset All button</button>
+        <ButtonComponent />
+        <ButtonComponent />
+        <ButtonComponent />
+        <ButtonComponent />
+      </div>
+    );
+  }
+}
 
 export default MainComponent;
